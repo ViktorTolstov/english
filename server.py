885e05e06d31b9b0903efc585b52f7d03bc7977b
ViktorTolstov <53541442+ViktorTolstov@users.noreply.github.com>
2020-02-13 19:57:01 +0700

    Update server.py

--- server.py
+++ server.py
@@ -208,7 +208,5 @@ def get_db():
 
 
 if __name__ == '__main__': 
-	#port = int(os.environ.get("PORT", 5000))
-	# app.run(host='0.0.0.0', port=port)
-    app.run()
-    # sched.start()
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
